0734e3edb703a7fbc4a87e783cb928197fafa483
Nana <nanalit16@gmail.com>
2024-04-08 11:14:50 +0300

    youtube

--- Components/YouTubeVideo/YouTubeVideo.js
+++ Components/YouTubeVideo/YouTubeVideo.js
@@ -2,44 +2,19 @@ import React from 'react';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import {View, Image, TouchableOpacity, Text} from 'react-native';
-import { Video, ResizeMode } from 'expo-av';
 import styled from './style.scss';
 import {icons} from "../../constants";
 import YoutubePlayer from "react-native-youtube-iframe";
 
-function YouTubeVideo() {
-    const video = React.useRef(null);
-    const [playing, setPlaying] = React.useState(false);
-
-    const onStateChange = React.useCallback((state) => {
-      if (state === "ended") {
-        setPlaying(false);
-      }
-    }, []);
-  
-    const togglePlaying = React.useCallback(() => {
-      setPlaying((prev) => !prev);
-    }, []);
-      
-        
+function YouTubeVideo({name, url}) {
   return (
         <View style={styled.videobox}>
                 <YoutubePlayer
                     webViewStyle={styled.video}
-                    height={200}
-                    play={playing}
-                    videoId={"NVIbCvfkO3E"}
-                    onChangeState={onStateChange}
+                    height={210}
+                    videoId={url}
                 /> 
-                {/* <TouchableOpacity style={[styled.play, {opacity: status.isPlaying ? 0 : 1}]}
-                        onPress={showVideoInFullscreen}
-                        >
-                <Image
-                    style={styled.image}
-                    source={icons.play}
-                />
-            </TouchableOpacity> */}
-            <Text style={[styled.text, {fontSize: RFValue ( 12 ,  740)}]}>Collective, Buiding B, 602 Dubai</Text>
+            <Text style={[styled.text, {fontSize: RFValue ( 12 ,  740)}]}>{name}</Text>
         </View>
     )
 }
